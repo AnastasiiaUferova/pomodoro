e7c6758ae0dc71ce0d55edd20039466c400935cb
‘Anastasiia <miss.iuferova@yandex.ru>
2022-12-08 18:16:17 +0100

    first setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SetPomodoro from './SetPomodoro';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pomodoro</h1>
+      <small>Be productive the right way</small>
+      <SetPomodoro />
+     
+    </div>
+  );
+}
+
+export default App;
